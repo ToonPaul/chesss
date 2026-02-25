9b3cfac3e3fdee5a5a565b55bfbd26de4f54e308
Toon Paul <hudallap@gmail.com>
2026-02-25 16:03:04 +0000

    piecestart

--- src/main/java/com/example/Piece.java
+++ src/main/java/com/example/Piece.java
@@ -61,7 +61,12 @@ public class Piece {
     //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
     //going to score any points.
     public ArrayList<Square> getLegalMoves(Board b, Square start){
-        
+        // b.getSquareArray();
+        // if(start.getCol() < 7){
+        // Square right = b.getSquareArray()[start.getRow()][start.getCol()+1];
+        // right.isOccupied();
+        // if(right.getOccupyingPiece().getColor()!= color()){}
+        // }
     	return null;
     }
 }
