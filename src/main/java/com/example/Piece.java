0511f3f0b686223540bb1e6f0e5b139eb097709b
Toon Paul <hudallap@gmail.com>
2026-02-25 15:28:59 +0000

    trwr

--- src/main/java/com/example/Piece.java
+++ src/main/java/com/example/Piece.java
@@ -61,6 +61,7 @@ public class Piece {
     //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
     //going to score any points.
     public ArrayList<Square> getLegalMoves(Board b, Square start){
+        
     	return null;
     }
 }
